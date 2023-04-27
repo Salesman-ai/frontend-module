0c0beb68f9c7af9e3b2976929d5757490d6299b9
JMielczarek02 <102308814+JMielczarek02@users.noreply.github.com>
2023-04-27 22:16:49 +0200

    fix axios post

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,18 @@
-import Form from './Form'
-import Result from './Result'
-import {useState} from 'react'
-const Main = () =>{
-    const [result,setResult]=useState();
-    const getResult = (data) =>{
-        setResult(data);
-    }
-return (
-<div className='row'>
-<Form result={getResult}/>
-<div className="right">
-         <Result result={result}/>
-</div>
-</div>
-)}
-export default Main
+import Form from "./Form";
+import Result from "./Result";
+import { useState } from "react";
+const Main = () => {
+  const [result, setResult] = useState();
+  const getResult = (data) => {
+    setResult(data);
+  };
+  return (
+    <div className="row">
+      <Form result={getResult} />
+      <div className="right">
+        <Result result={result} />
+      </div>
+    </div>
+  );
+};
+export default Main;
