9ae76babfabb1a7081f77e06c5fb551de91edc48
236639@edu.p.lodz.pl <236639@edu.p.lodz.pl>
2023-04-27 21:21:51 +0200

    change post->get

--- src/components/Form.js
+++ src/components/Form.js
@@ -70,7 +70,7 @@ const Form = (props) => {
   };
   async function sendRequest(car) {
     axios
-      .post("http://192.168.58.4:8080/api-backend/get-price", car)
+      .get("http://192.168.58.4:8080/api-backend/get-price", car)
       .then((response) => props.result(response))
       .catch((error) => console.log(error));
   }
